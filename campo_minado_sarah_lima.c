5ef0977452e58069b5a72db0c510536ec19d31f0
Sarah Lima <sarah.lima@icomp.ufam.edu.br>
2024-10-16 00:10:00 -0400

    tentativa de impl da func initializeMat c erros

--- campo_minado_sarah_lima.c
+++ campo_minado_sarah_lima.c
@@ -2,41 +2,77 @@
 #include <stdlib.h>
 #include <string.h>
 
-//Criando nova matriz
-char** newMatrix(int lin, int col){
-    char **mat = calloc(lin,sizeof(char*));
+//Creating a new matrix
+char** newMatrix(int* lin, int* col){
+    char **mat = calloc(*lin,sizeof(char*));
 
-    for(int ind = 0; ind < lin; ind++){
-        mat[ind] = (char*) calloc(col,sizeof(char));
+    if(mat == NULL){
+        printf("ERROR01! Insufficient memory!");
+        exit(1);
     }
-    
+
+    for(int ind = 0; ind < *lin; ind++){
+        mat[ind] = (char*) calloc(*col,sizeof(char));
+
+        if(mat == NULL){
+        printf("ERROR02! Insufficient memory!");
+            exit(1);
+        }
+    }
     return mat;
 }
 
-//Inicializando matriz
-char** initializeMat(char** mat){
+//Initializing the matrix mat
+char** initializeMat(char** mat, int nLin, int nCol){
 
+    for(int line = 0; line < nLin; line++){
+        for(int column = 0; column < nCol; column++){
+            mat[line,column] = 'x';
+        }
+    }
     return mat;
 } 
-//Escolhe nivel do jogo
-void gameLevel(){
-    char level[8];
-    scanf("%s", level);
+void printMatrix(char** mat, int lin, int col){
 
-    if((strcmp(level,"facil")) == 0){
-        newMatrix(10,10);
-    }
-    else if((strcmp(level,"medio")) == 0){
-        newMatrix(20,20);
-    }
-    else if((strcmp(level,"dificil")) == 0){
-        newMatrix(30,30);
-    }
-    else{
-        printf("Level not found =( \n Please, enter a valid level!!!");
+}
+//Escolhe nivel do jogo
+void gameLevel(int *lin, int *col){
+    char level[8];
+
+    while(1){
+        scanf("%s", level);
+
+        if((strcmp(level,"facil")) == 0){
+            *lin = 10;
+            *col = 10;
+            printf("deu certo");
+            break;
+        }
+        else if((strcmp(level,"medio")) == 0){
+            *lin = 20;
+            *col = 20;
+            break;
+        }
+        else if((strcmp(level,"dificil")) == 0){
+            *lin = 30;
+            *col = 30;
+            break;
+        }
+        else{
+            printf("ERROR! level not found =( \nPlease, enter a valid level!!!\n\n");
+        }
+        newMatrix(lin,col);
     }
 }
 int main() {
+    int lin;
+    int col;
+
+    char mat = newMatrix(&lin,&col);
+
+    gameLevel(&lin, &col);
+//We call the funcion "initializeMatrix" to initializate the matrix mat
+    initializeMat(&mat, &lin, &col);
    
 
     return 0;
